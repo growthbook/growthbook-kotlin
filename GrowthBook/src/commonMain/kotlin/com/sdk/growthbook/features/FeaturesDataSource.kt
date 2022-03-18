ff9981de086bcc976ed14011ac5c3647341f4904
v.stelmashchuk <volodymyr.stelmashchuk@pm.bet>
2022-03-18 13:17:03 +0200

    Fixed test, isolate FeatureUrlBulder from GB context

--- GrowthBook/src/commonMain/kotlin/com/sdk/growthbook/features/FeaturesDataSource.kt
+++ GrowthBook/src/commonMain/kotlin/com/sdk/growthbook/features/FeaturesDataSource.kt
@@ -1,6 +1,7 @@
 package com.sdk.growthbook.features
 
 import com.sdk.growthbook.GrowthBookSDK
+import com.sdk.growthbook.model.GBContext
 import com.sdk.growthbook.network.CoreNetworkClient
 import com.sdk.growthbook.network.NetworkDispatcher
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -9,9 +10,13 @@ import kotlinx.serialization.decodeFromString
 /**
  * DataSource for Feature API
  */
-internal class FeaturesDataSource(val dispatcher: NetworkDispatcher = CoreNetworkClient()) {
+internal class FeaturesDataSource(
+    private val dispatcher: NetworkDispatcher = CoreNetworkClient(),
+    private val featureURLBuilder: FeatureURLBuilder = FeatureURLBuilder(),
+    private val gbContext: GBContext = GrowthBookSDK.gbContext
+) {
 
-    private val apiUrl = FeatureURLBuilder().buildUrl(GrowthBookSDK.gbContext)
+    private val apiUrl = featureURLBuilder.buildUrl(gbContext.hostURL, gbContext.apiKey)
 
     /**
      * Executes API Call to fetch features
--- GrowthBook/src/commonTest/kotlin/com/sdk/growthbook/features/FeatureURLBuilderTest.kt
+++ GrowthBook/src/commonTest/kotlin/com/sdk/growthbook/features/FeatureURLBuilderTest.kt
@@ -1,6 +1,5 @@
 package com.sdk.growthbook.features
 
-import com.sdk.growthbook.model.GBContext
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -9,7 +8,7 @@ class FeatureURLBuilderTest {
 	@Test
 	fun verifyCreateUrl() {
 		val actual = FeatureURLBuilder()
-				.buildUrl(gBContext = buildGbContext("https://some.domain", "api_key"))
+				.buildUrl("https://some.domain", "api_key")
 
 		assertEquals("https://some.domain/api/features/api_key", actual)
 	}
@@ -17,20 +16,8 @@ class FeatureURLBuilderTest {
 	@Test
 	fun verifyCreateUrlWithDash() {
 		val actual = FeatureURLBuilder()
-				.buildUrl(gBContext = buildGbContext("https://some.domain/", "api_key_2"))
+				.buildUrl("https://some.domain/", "api_key_2")
 
 		assertEquals("https://some.domain/api/features/api_key_2", actual)
 	}
-
-	private fun buildGbContext(hostUrl: String, apiKey: String): GBContext {
-		return GBContext(
-				apiKey = apiKey,
-				hostURL = hostUrl,
-				enabled = true,
-				attributes = emptyMap(),
-				forcedVariations = emptyMap(),
-				qaMode = true,
-				trackingCallback = { _, _ -> })
-	}
-
 }
