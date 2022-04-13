ea21aff19dc23f41cf15f7152d008c9fafe677d5
Volodymyr <vovochkastelmashchuk@gmail.com>
2022-04-13 16:50:41 +0300

    Fixed NetworkDispatcher for IOS build (#20)
    Co-authored-by: v.stelmashchuk <volodymyr.stelmashchuk@pm.bet>

--- GrowthBook/src/commonMain/kotlin/com/sdk/growthbook/features/FeaturesDataSource.kt
+++ GrowthBook/src/commonMain/kotlin/com/sdk/growthbook/features/FeaturesDataSource.kt
@@ -5,11 +5,15 @@ import com.sdk.growthbook.Network.CoreNetworkClient
 import com.sdk.growthbook.Network.NetworkDispatcher
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 /**
  * DataSource for Feature API
  */
 internal class FeaturesDataSource(val dispatcher: NetworkDispatcher = CoreNetworkClient()) {
+
+    private val JSONParser: Json
+        get() = Json { prettyPrint = true; isLenient = true; ignoreUnknownKeys = true }
 
   private val apiUrl =
 	FeatureURLBuilder().buildUrl(GrowthBookSDK.gbContext.hostURL, GrowthBookSDK.gbContext.apiKey)
@@ -23,7 +27,7 @@ internal class FeaturesDataSource(val dispatcher: NetworkDispatcher = CoreNetwor
   ) {
 	dispatcher.consumeGETRequest(apiUrl,
 	  onSuccess = { rawContent ->
-		val result: FeaturesDataModel = dispatcher.JSONParser.decodeFromString(rawContent)
+		val result: FeaturesDataModel = JSONParser.decodeFromString(rawContent)
 		result.also(success)
 	  },
 	  onError = { apiTimeError ->
