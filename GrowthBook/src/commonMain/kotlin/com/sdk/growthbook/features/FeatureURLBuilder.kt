ecfffa06482e2167b6b5514e5240f8157604956c
Volodymyr Nazarkevych <volodymyr.nazarkevych@kevychsolutions.com>
2023-06-07 19:17:56 +0300

    Failing tests was solved and deprecated method was got rid.

--- GrowthBook/src/commonMain/kotlin/com/sdk/growthbook/features/FeatureURLBuilder.kt
+++ GrowthBook/src/commonMain/kotlin/com/sdk/growthbook/features/FeatureURLBuilder.kt
@@ -1,21 +1,20 @@
 package com.sdk.growthbook.features
 
-import io.ktor.http.URLBuilder
-import io.ktor.http.Url
-import io.ktor.http.pathComponents
-
 internal class FeatureURLBuilder {
 
     companion object {
         /**
          * Context Path for Fetching Feature Details - Web Service
          */
-        private const val featurePath = "api/features/"
+        private const val featurePath = "api/features"
     }
 
     fun buildUrl(baseUrl: String, apiKey: String): String {
-        return URLBuilder(url = Url(baseUrl))
-            .pathComponents(featurePath, apiKey)
-            .buildString()
+        val baseUrlWithFeaturePath = if (baseUrl.endsWith('/'))
+            "$baseUrl$featurePath"
+        else
+            "$baseUrl/$featurePath"
+
+        return "$baseUrlWithFeaturePath/$apiKey"
     }
 }
