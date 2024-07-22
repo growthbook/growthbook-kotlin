d91b55287bc3152ec651339aa253cfc3756841c0
Bohdan Akimenko <bohdan.akimenko@kevychsolutions.com>
2024-07-22 12:36:38 +0300

    Dokka plugin version was updated

--- GrowthBook/build.gradle.kts
+++ GrowthBook/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     kotlin("plugin.serialization")
     id("maven-publish")
     id("signing")
-    id("org.jetbrains.dokka") version "1.4.20"
+    id("org.jetbrains.dokka") version "1.8.10"
 }
 
 group = "io.growthbook.sdk"
