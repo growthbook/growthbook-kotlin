8d4d4183fd05739be074dda8cb23b7be56f57f5a
Bohdan Akimenko <bohdan.akimenko@kevychsolutions.com>
2024-09-19 16:50:30 +0300

    ios simulator target was added

--- Core/build.gradle.kts
+++ Core/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
@@ -22,11 +24,14 @@ kotlin {
     }
 
     ios()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
 
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
             }
         }
     }
