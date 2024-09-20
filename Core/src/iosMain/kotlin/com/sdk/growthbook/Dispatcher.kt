b1a10233196a708927d91065ffa6fa20acc23e8b
Bohdan Akimenko <bohdan.akimenko@kevychsolutions.com>
2024-09-20 14:47:37 +0300

    Trying to get ios binaries published

--- Core/src/iosMain/kotlin/com/sdk/growthbook/Dispatcher.kt
+++ Core/src/iosMain/kotlin/com/sdk/growthbook/Dispatcher.kt
@@ -1,8 +1,8 @@
 package com.sdk.growthbook
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
+//import kotlinx.coroutines.CoroutineDispatcher
+//import kotlinx.coroutines.Dispatchers
 
 // 1
-actual val PlatformDependentIODispatcher: CoroutineDispatcher =
-    Dispatchers.Default
+//actual val PlatformDependentIODispatcher: CoroutineDispatcher =
+//    Dispatchers.Default
