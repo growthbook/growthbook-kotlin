c25e2cfa235e0716ce3648cae89f40092343c9f5
Braden Farmer <farmerbb@gmail.com>
2024-10-31 10:12:53 -0600

    Revert AGP to 7.4.2 and JDK to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.1")
+        classpath("com.android.tools.build:gradle:7.4.2")
 
         val kotlinVersion = "1.9.24"
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
