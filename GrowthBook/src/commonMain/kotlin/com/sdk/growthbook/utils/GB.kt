7306e6e0569c751a0c077d3fd6cb6a8ff26003f7
Bohdan Akimenko <bohdan.akimenko@kevychsolutions.com>
2025-05-22 16:53:58 +0300

    add log

---
+++ GrowthBook/src/commonMain/kotlin/com/sdk/growthbook/utils/GB.kt
@@ -0,0 +1,10 @@
+package com.sdk.growthbook.utils
+
+object GB {
+
+    private const val LOG_TAG = "GrowthBook log:"
+
+    fun log(message: String) {
+        println("$LOG_TAG $message")
+    }
+}
