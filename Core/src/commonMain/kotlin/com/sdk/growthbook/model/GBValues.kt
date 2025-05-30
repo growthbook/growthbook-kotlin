3a5e262d6fac4ddd64e4f770c5cd8de9edeec075
Andrey Koshmanskiy <andrew.k.21.12@gmail.com>
2025-05-30 16:00:00 +0300

    Drop floating-point error support for GBNumber

--- Core/src/commonMain/kotlin/com/sdk/growthbook/model/GBValues.kt
+++ Core/src/commonMain/kotlin/com/sdk/growthbook/model/GBValues.kt
@@ -1,8 +1,5 @@
 package com.sdk.growthbook.model
 
-import kotlin.math.abs
-import kotlin.math.roundToLong
-
 data object GBNull: GBValue()
 data class GBBoolean(val value: Boolean): GBValue()
 data class GBString(val value: String): GBValue()
@@ -21,11 +18,7 @@ class GBNumber(val value: Number): GBValue() {
         }
         if (
             !isIntegerValue() && !other.isIntegerValue() &&
-            (
-                value.toDouble() == other.value.toDouble() ||
-                // Handles rare cases when attributes are computed with floating-point errors.
-                abs(value.toDouble() - other.value.toDouble()) < DOUBLE_COMPARISON_EPSILON
-            )
+            value.toDouble() == other.value.toDouble()
         ) {
             return true
         }
@@ -36,30 +29,13 @@ class GBNumber(val value: Number): GBValue() {
         if (isIntegerValue()) {
             value.toLong().hashCode()
         } else {
-            // Reduces precision of hash codes
-            // to respect hash code equality for numbers with floating-point errors.
-            (
-                (value.toDouble() * DOUBLE_COMPARISON_PRECISION).roundToLong() /
-                    DOUBLE_COMPARISON_PRECISION
-            ).hashCode()
+            value.toDouble().hashCode()
         }
 
     override fun toString(): String = "GBNumber(value=$value)"
 
     private fun isIntegerValue(): Boolean =
         value is Byte || value is Short || value is Int || value is Long
-
-    private companion object {
-
-        private const val DOUBLE_COMPARISON_PRECISION: Double = 10000000.0
-
-        /**
-         * Should be smaller than `1 / `[DOUBLE_COMPARISON_PRECISION]
-         * to guarantee hash code equality for equal numbers with floating-point errors.
-         */
-        private const val DOUBLE_COMPARISON_EPSILON: Double = 0.9 / DOUBLE_COMPARISON_PRECISION
-
-    }
 
 }
 
--- GrowthBook/src/jvmTest/kotlin/com/sdk/growthbook/tests/GBNumberTests.kt
+++ GrowthBook/src/jvmTest/kotlin/com/sdk/growthbook/tests/GBNumberTests.kt
@@ -2,6 +2,7 @@ package com.sdk.growthbook.tests
 
 import com.sdk.growthbook.model.GBArray
 import com.sdk.growthbook.model.GBNumber
+import com.sdk.growthbook.model.toGbNumber
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -9,31 +10,40 @@ import kotlin.test.assertTrue
 class GBNumberTests {
 
     private val integerNumbers: List<GBNumber> =
-        listOf(123, 123L, 123.toByte(), 123.toShort()).map(::GBNumber)
+        listOf(
+            123.toGbNumber(),
+            123L.toGbNumber(),
+            123.toByte().toGbNumber(),
+            123.toShort().toGbNumber()
+        )
 
-    private val floatNumbers: List<GBNumber> = listOf(123f, 123.0).map(::GBNumber)
-
-    private val floatingPointErrorNumbers: List<GBNumber> =
-        listOf(0.1 + 0.2, 0.3, 0.1f + 0.2f).map(::GBNumber)
+    private val floatNumbers: List<GBNumber> =
+        listOf(
+            123f.toGbNumber(),
+            123.0.toGbNumber()
+        )
 
     @Test
     fun testEquals() {
         assertAllPairsEqual(integerNumbers)
         assertAllPairsEqual(floatNumbers)
-        assertAllPairsEqual(floatingPointErrorNumbers)
     }
 
     @Test
     fun testHashCode() {
         assertAllPairsEqual(integerNumbers.map { it.hashCode() })
         assertAllPairsEqual(floatNumbers.map { it.hashCode() })
-        assertAllPairsEqual(floatingPointErrorNumbers.map { it.hashCode() })
     }
 
     @Test
     fun testGrowthBookArrayContains() {
-        floatingPointErrorNumbers.forEach { number1 ->
-            floatingPointErrorNumbers.forEach { number2 ->
+        integerNumbers.forEach { number1 ->
+            integerNumbers.forEach { number2 ->
+                assertTrue(GBArray(listOf(number1)).contains(number2))
+            }
+        }
+        floatNumbers.forEach { number1 ->
+            floatNumbers.forEach { number2 ->
                 assertTrue(GBArray(listOf(number1)).contains(number2))
             }
         }
