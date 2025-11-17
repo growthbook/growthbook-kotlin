315e54f1fb99d592801b3ad0da132db4e1d937cd
Bohdan Akimenko <bohdan.akimenko@kevychsolutions.com>
2025-11-17 21:44:06 +0200

    Fix "None of the following functions can be called with the arguments supplied"

--- GrowthBook/src/iosMain/kotlin/com/sdk/growthbook/utils/Base64.ios.kt
+++ GrowthBook/src/iosMain/kotlin/com/sdk/growthbook/utils/Base64.ios.kt
@@ -1,22 +1,22 @@
 package com.sdk.growthbook.utils
 
+import platform.posix.memcpy
 import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.usePinned
 import platform.Foundation.NSData
-import platform.Foundation.NSDataBase64DecodingIgnoreUnknownCharacters
 import platform.Foundation.create
-import platform.posix.memcpy
+import platform.Foundation.NSDataBase64DecodingIgnoreUnknownCharacters
 
 internal actual fun decodeBase64(base64: String): ByteArray {
-    val nsData = NSData.create(base64 = base64, options = NSDataBase64DecodingIgnoreUnknownCharacters)
+    val nsData = NSData.create(base64EncodedString = base64, options = NSDataBase64DecodingIgnoreUnknownCharacters)
         ?: throw IllegalArgumentException("Invalid Base64 string")
     
     val byteArray = ByteArray(nsData.length.toInt())
     if (nsData.length > 0u) {
+        @OptIn(kotlinx.cinterop.ExperimentalForeignApi::class)
         byteArray.usePinned { pinned ->
             memcpy(pinned.addressOf(0), nsData.bytes, nsData.length)
         }
     }
     return byteArray
 }
-
