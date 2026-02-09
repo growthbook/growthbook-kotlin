2a27284f50541d5b8b536466895f4afc3da508f8
Volodymyr Nazarkevych <volodymyr.nazarkevych@kevychsolutions.com>
2026-02-09 12:59:57 +0200

    fix: remove manual Accept-Encoding header in OkHttp and Ktor dispatchers

--- NetworkDispatcherKtor/src/commonMain/kotlin/com/sdk/growthbook/network/GBNetworkDispatcherKtor.kt
+++ NetworkDispatcherKtor/src/commonMain/kotlin/com/sdk/growthbook/network/GBNetworkDispatcherKtor.kt
@@ -149,7 +149,6 @@ class GBNetworkDispatcherKtor(
                         append("If-None-Match", it)
                     }
                     append("Cache-Control", "max-age=3600")
-                    append("Accept-Encoding", "gzip, deflate, br")
                 }
             }
             queryParams.forEach { (key, value) -> addOrReplaceParameter(key, value) }
