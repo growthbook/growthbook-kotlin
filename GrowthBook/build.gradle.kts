bca7896956f9596a02b64a903bf4380ad277aae6
Volodymyr Nazarkevych <volodymyr.nazarkevych@kevychsolutions.com>
2026-09-02 11:15:32 +0300

    chore: prepare changelog and bump version

--- GrowthBook/build.gradle.kts
+++ GrowthBook/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "io.growthbook.sdk"
-version = "7.8.0"
+version = "7.8.1"
 
 val generateSdkMeta by tasks.registering {
     val outputDir = layout.buildDirectory.dir("generated/sdk-meta/commonMain/kotlin")
